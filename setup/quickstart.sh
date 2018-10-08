6ccc3c189d75e306a8e6ccad8f438899a8d57f38
Andreas Falk <andreas.falk@novatec-gmbh.de>
2018-10-08 08:01:40 +0200

    remove presentation and update demos

--- setup/quickstart.sh
+++ setup/quickstart.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-sudo /home/afa/scripts/concourse quickstart \
-  --basic-auth-username dev \
-  --basic-auth-password dev \
-  --worker-work-dir /home/afa/concourse/worker \
-  --postgres-user concourse \
-  --postgres-password concourse \
-  --worker-garden-dns-server 1.1.1.1
+docker-compose up
 
 
