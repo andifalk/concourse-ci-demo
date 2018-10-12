97638d18f39073b9a881e5f1f544e0e59fc15db4
Andreas Falk <andreas.falk@novatec-gmbh.de>
2018-10-12 15:25:21 +0200

    update build

--- setup/init_credhub_values.sh
+++ setup/init_credhub_values.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 credhub login -s https://localhost:9000 -u credhub -p password --skip-tls-validation
+
+credhub set --name=/concourse/cf-user --value=admin --type=value
+credhub set --name=/concourse/cf-password --value=admin --type=value
+credhub find /concourse
