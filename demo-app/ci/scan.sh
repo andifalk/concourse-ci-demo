0f6d07ecf59078cee42284dc5660cb427b203b15
Andreas Falk <andreas.falk@novatec-gmbh.de>
2018-11-19 23:29:02 +0100

    update pipeline

---
+++ demo-app/ci/scan.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+zap-cli start --start-options '-config api.disablekey=true'
+zap-cli status -t 60
+#zap-cli context import demoapp.context
+zap-cli open-url "http://concourse-demo.local.pcfdev.io/"
+#zap-cli spider --context-name DevTest --user-name SomeUser "http://concourse-demo.local.pcfdev.io/"
+#zap-cli active-scan --recursive -c DevTest -u SomeUser "http://concourse-demo.local.pcfdev.io/"
+zap-cli spider "http://concourse-demo.local.pcfdev.io/"
+zap-cli active-scan --recursive --scanners all "http://concourse-demo.local.pcfdev.io/"
+zap-cli alerts --alert-level Informational
+zap-cli shutdown
