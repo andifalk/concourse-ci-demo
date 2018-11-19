8c6d7ade280ad35085ca9a4043653401a7dae399
Andreas Falk <andreas.falk@novatec-gmbh.de>
2018-11-19 23:59:54 +0100

    add exit code

--- demo-app/ci/scan.sh
+++ demo-app/ci/scan.sh
@@ -8,5 +8,6 @@ zap-cli open-url "http://concourse-demo.local.pcfdev.io/"
 #zap-cli active-scan --recursive -c DevTest -u SomeUser "http://concourse-demo.local.pcfdev.io/"
 zap-cli spider "http://concourse-demo.local.pcfdev.io/"
 zap-cli active-scan --recursive --scanners all "http://concourse-demo.local.pcfdev.io/"
-zap-cli alerts --alert-level Low
+ip=$(zap-cli alerts --alert-level Low)
 zap-cli shutdown
+exit $ip
